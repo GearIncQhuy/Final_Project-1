f86072c4dc63cd3e624eb626fe724a5c851d6367
hoanganhkhonghoc <hoanganhkhonghoc@gmail.com>
2023-10-09 22:04:23 +0700

    HoangAnh_Card.
    Chức năng card có thể random và chọn. khi chọn thì sẽ chuyển level tiếp theo và tính dame cộng dồn.

---
+++ Assets/Game/Scripts/Script Table/Card/ScriptTableCard.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "Data/Card")]
+public class ScriptTableCard : ScriptableObject
+{
+    [Header("Icon")]
+    public Sprite icon;
+    [Header("Poperties")]
+    public float dame;
+    public float speedFire;
+    public float speed;
+    public float mana;
+    public float heal;
+    public int checkUse;
+    [Header("Elemental")]
+    public Phases phases;
+    public Sprite sprite;
+}
---
+++ Assets/Game/Scripts/UI/Card/Card.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Card
+{
+    public Sprite icon;
+    public float dame;
+    public float speedFire;
+    public float speed;
+    public float mana;
+    public float heal;
+    public int checkUse;
+    public Phases phases;
+    public Sprite sprite;
+
+    private ScriptTableCard cardCurrent;
+
+    public Card(ScriptTableCard card)
+    {
+        this.icon = card.icon;
+        this.dame = card.dame;
+        this.speedFire = card.speedFire;
+        this.speed = card.speed;
+        this.mana = card.mana;
+        this.heal = card.heal;
+        this.checkUse = card.checkUse;
+        this.phases = card.phases;
+        this.sprite = card.sprite;
+        cardCurrent = card;
+    }
+
+    public void UpdateUse()
+    {
+        cardCurrent.checkUse++;
+    }
+}
